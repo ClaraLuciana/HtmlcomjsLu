293fb5807714b3c7f42ada7199e993ad6860b056
Luciana Clara Silva <114404204+ClaraLuciana@users.noreply.github.com>
2023-10-19 20:23:26 -0300

    19.10

--- main.js
+++ main.js
@@ -1,8 +1,18 @@
-//Pom .....................
-function tocaSomPom () {
-    document.querySelector('#som_tecla_pom').play();
-}
-
-const listaDeTeclas = document.querySelectorAll('.tecla');
-
-listaDeTeclas[0].onclick = tocaSomPom;
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+ }
+ 
+ const listaDeTeclas = document.querySelectorAll('.tecla');
+ 
+ let contador = 0;
+ 
+ //enquanto
+ while (contador < listaDeTeclas.length) {
+     listaDeTeclas[contador].onclick = function ()  {
+         tocaSom('#som_tecla_pom')
+     };
+     
+     contador = contador + 1;
+ 
+     console.log(contador);
+ }
